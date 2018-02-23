fed79b66fdf73ebc726c44eb68f6291d0a7ce415
talyh <taly.hotimsky@gmail.com>
2018-02-22 22:54:11 -0500

    Promises exercise 2 completed

--- promises/exercise-2.js
+++ promises/exercise-2.js
@@ -3,10 +3,10 @@ require("es6-promise");
 // Create a promise that after a delay of 300ms, rejects with an Error object. The Error object should be constructed with parameter 'REJECTED!', which is the textual message of the error.
 // Create a function onReject to print error.message using console.log. Pass this function as a rejection handler to the then method of your promise.
 
-const promise = new Promise(function(resolve, reject) {
-  // Your solution here
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error("REJECTED")), 3000)
 });
 
-const onReject = value => {
-  // Your solution here
-};
+const onReject = error => console.log("error onReject: ", error.message);
+
+promise.catch(onReject)
