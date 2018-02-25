dae82414ab9cc2cfabe89869425873f4303f9a15
Tahuana <tahuana@gmail.com>
2018-02-25 12:54:32 -0500

    Solivng exercise-3.js

--- promises/exercise-3.js
+++ promises/exercise-3.js
@@ -1,3 +1,11 @@
 const fetch = require('node-fetch');
 
 // Make a fetch request to https://next.json-generator.com/api/json/get/EJPkuFBIV
+
+fetchJson = () => 
+    fetch('https://next.json-generator.com/api/json/get/EJPkuFBIV')
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(error => console.log(error) )
+
+fetchJson();
