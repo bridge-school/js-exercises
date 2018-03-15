9acc52c95fcb1b14fa83c2e0f0c4cd92e05c33af
Uma Chandran <uma.chandran@rangle.io>
2018-03-15 11:25:23 -0400

    adds observables intro homework

---
+++ observables/exercise-1/observables.js
@@ -0,0 +1,10 @@
+const { Observable } = Rx;
+
+const theButton = document.getElementById("exercise-1");
+const clickStream = Observable.fromEvent(theButton, "click");
+
+/* 
+  Exercise 1:
+  We've created an Observable stream from the button's click event above. 
+  Subscribe to the stream and print out the emitted value.
+*/
