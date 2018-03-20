900d9120588c2547b239e620fd454eff84bd66d2
talyh <taly.hotimsky@gmail.com>
2018-03-20 19:12:42 -0400

    Observables Ex1 completed

--- observables/exercise-1/observables.js
+++ observables/exercise-1/observables.js
@@ -8,3 +8,5 @@ const clickStream = Observable.fromEvent(theButton, "click");
   We've created an Observable stream from the button's click event above. 
   Subscribe to the stream and print out the emitted value.
 */
+
+clickStream.subscribe(val => console.log(val));
