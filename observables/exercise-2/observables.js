5ab455a6985a6f39fc864830d834f4c47d46c596
rosexw <rose.xw.wan@gmail.com>
2018-03-26 16:13:56 -0400

    observables 2

--- observables/exercise-2/observables.js
+++ observables/exercise-2/observables.js
@@ -3,10 +3,10 @@ const { Observable } = Rx;
 const theButton = document.getElementById("exercise-2");
 const clickStream = Observable.fromEvent(theButton, "click");
 
-/* 
+/*
   Exercise 2:
-  We've created an Observable stream from the button's click event above. We want each click to give us a "dog". 
-  Use one of the RxJS methods to map each emitted value from a click event to a string instead. The string should tell us which dog number we're at. 
+  We've created an Observable stream from the button's click event above. We want each click to give us a "dog".
+  Use one of the RxJS methods to map each emitted value from a click event to a string instead. The string should tell us which dog number we're at.
   Subscribe to the stream and print out the emitted value.
 
   Expected output:
@@ -14,3 +14,7 @@ const clickStream = Observable.fromEvent(theButton, "click");
 */
 
 
+
+clickStream
+.map((value, index) => `Dog #${index+1}`)
+.subscribe(dog => console.log(dog));
